d4442f0cf0bcc6be269cdb21172c12a5281c2a75
Tero Marttila <tero.marttila@kontena.io>
2016-12-14 14:06:06 +0200

    Kontena::Registrator::Service: skydns example

--- lib/kontena/registrator/main.rb
+++ lib/kontena/registrator/main.rb
@@ -2,8 +2,11 @@ require 'celluloid'
 
 module Kontena::Registrator
   require 'kontena/registrator/docker'
+  require 'kontena/registrator/service'
 
   class Main < Celluloid::Supervision::Container
     supervise type: Docker::Actor, as: :docker
+
+    supervise type: Service, as: :skydns, args: [Docker::Actor.observable, Service::SKYDNS]
   end
 end
---
+++ lib/kontena/registrator/service.rb
@@ -0,0 +1,92 @@
+require 'celluloid'
+require 'kontena/logging'
+require 'kontena/registrator/etcd'
+
+module Kontena::Registrator
+  class Service
+    include Kontena::Logging
+    include Celluloid
+
+    # SkyDNS example
+    SKYDNS = {
+      where: [
+        proc { |container| container.networks.has_key? 'kontena' },
+        proc { |container| !container.networks['kontena']['IPAddress'].empty? },
+      ],
+      select: {
+        kontena_ip: proc { |container| container.networks['kontena']['IPAddress'] }
+      },
+      etcd: {
+        path: [
+          '/skydns',
+          'kontena.local'.split('.').reverse,
+          proc { |container| container.hostname },
+        ],
+        value: {
+          host: proc { |container, kontena_ip: | kontena_ip },
+        }
+      }
+    }
+
+    def initialize(docker_observable, config)
+      @etcd_writer = Etcd::Writer.new
+      @config = config
+
+      self.async.run(docker_observable)
+    end
+
+    def eval_container(container)
+      context = { }
+
+      unless networks = container.networks
+        logger.error "container does not nave networks: #{container.inspect}"
+      end
+
+      if where = @config[:where]
+        where.each do |expr|
+          if value = container.eval(expr, context)
+            logger.debug "where=#{expr} match value=#{value}"
+          else
+            logger.debug "where=#{expr} skip value=#{value}"
+            return
+          end
+        end
+      end
+
+      if select = @config[:select]
+        select.each_pair do |sym, expr|
+          context[sym] = container.eval(expr, context)
+        end
+      end
+
+      if etcd_config = @config[:etcd]
+        path = [container.eval(etcd_config[:path], context)].flatten.join '/'
+        value = container.eval(etcd_config[:value], context).to_json
+
+        logger.debug "eval container: path=#{path} value=#{value.inspect}"
+
+        yield path, value if path && value
+      end
+    end
+
+    def update(docker_state)
+      logger.info "Update..."
+      nodes = {}
+      docker_state.containers.each do |container|
+        self.eval_container(container) do |path, value|
+          nodes[path] = value
+        end
+      end
+
+      @etcd_writer.write(nodes)
+    end
+
+    def run(docker_observable)
+      logger.debug "observing #{docker_observable}"
+
+      docker_observable.observe do |docker_state|
+        self.update(docker_state)
+      end
+    end
+  end
+end
