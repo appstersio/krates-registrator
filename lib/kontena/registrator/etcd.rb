57562d79dbfd0d94620f5239ee375793becfeb3e
Tero Marttila <tero.marttila@kontena.io>
2016-12-14 14:05:52 +0200

    Kontena::Registrator::Etcd: Writer

---
+++ lib/kontena/registrator/etcd.rb
@@ -0,0 +1,32 @@
+require 'kontena/logging'
+require 'kontena/etcd/client'
+
+module Kontena::Registrator::Etcd
+  class Writer
+    include Kontena::Logging
+
+    def initialize
+      @nodes = { }
+      @client = Kontena::Etcd::Client.new
+
+      logger.info "Connected to etcd=#{@client.uri} with version=#{@client.version}"
+    end
+
+    # Update set of path => value nodes to etcd
+    def write(nodes)
+      nodes.each_pair do |key, value|
+        if value != @nodes[key]
+          @client.set(key, value: value)
+        end
+      end
+
+      @nodes.each do |key, value|
+        if !nodes[key]
+          @client.delete(key)
+        end
+      end
+
+      @nodes = nodes
+    end
+  end
+end
