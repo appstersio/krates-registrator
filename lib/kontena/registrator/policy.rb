cae835f3f3f295766eb2146ea2575ff2aa3e2081
Tero Marttila <tero.marttila@kontena.io>
2016-12-22 14:19:39 +0200

    lib/kontena/registrator: revert back to top-level classes in .rb files, it works correctly via the top-level require 'kontena/registrator'

--- lib/kontena/registrator/policy.rb
+++ lib/kontena/registrator/policy.rb
@@ -1,90 +1,86 @@
 require 'kontena/logging'
 
-module Kontena
-  class Registrator
-    # A schema for selecting Docker containers and registering etcd configuration nodes
-    class Policy
-      include Kontena::Logging
+# A schema for selecting Docker containers and registering etcd configuration nodes
+class Kontena::Registrator::Policy
+  include Kontena::Logging
 
-      # @param path [String] filesystem path to .rb file
-      def self.load(path)
-        name = File.basename(path)
-        policy = new(name)
+  # @param path [String] filesystem path to .rb file
+  def self.load(path)
+    name = File.basename(path)
+    policy = new(name)
 
-        File.open(path, "r") do |file|
-          policy.load(file)
-        end
-
-        policy
-      end
-
-      attr_accessor :name, :context
-
-      def initialize(name)
-        @name = name
-        @context = Context.new
-      end
-
-      # Evaluate .rb DSL
-      #
-      # @param file [File]
-      def load(file)
-         @context.instance_eval(file.read, file.path)
-      end
-
-      # Evaluation context for DSL
-      class Context
-        def [](sym)
-          instance_variable_get("@#{sym}")
-        end
-
-        # Register a Docker::Container handler
-        #
-        # * returning nil is equivalent to returning an empty Hash
-        # * nil values are elided
-        # * String values are kept as raw strings
-        # * Object values are encoded to JSON
-        #
-        # @param proc [Proc] Kontena::Registrator::Docker::Container -> Hash{String => nil, String, JSON>
-        def docker_container(proc)
-          @docker_container = proc
-        end
-      end
-
-      # Normalize policy nodes to etcd nodes
-      #
-      # @param nodes [Hash{String => nil, String, JSON}]
-      # @return nodes [Hash{String => String}]
-      def apply_nodes(nodes)
-        return {} if nodes.nil?
-
-        Hash[nodes.map{|key, value|
-          case value
-          when nil
-            next
-          when String
-            [key, value]
-          when true, false, Integer, Float, Array, Hash
-            [key, value.to_json]
-          else
-            raise TypeError, "Invalid value for etcd #{key}: #{value.inspect}"
-          end
-        }]
-      end
-
-      # Compile a Docker::State into a set of etcd nodes
-      #
-      # @param state [Kontena::Registrator::Docker::State]
-      # @return [Hash<String, String>] nodes for Kontena::Etcd::Writer
-      def apply(state)
-        nodes = {}
-
-        state.containers.each do |container|
-          nodes.merge! apply_nodes @context[:docker_container].call(container)
-        end
-
-        nodes
-      end
+    File.open(path, "r") do |file|
+      policy.load(file)
     end
+
+    policy
+  end
+
+  attr_accessor :name, :context
+
+  def initialize(name)
+    @name = name
+    @context = Context.new
+  end
+
+  # Evaluate .rb DSL
+  #
+  # @param file [File]
+  def load(file)
+     @context.instance_eval(file.read, file.path)
+  end
+
+  # Evaluation context for DSL
+  class Context
+    def [](sym)
+      instance_variable_get("@#{sym}")
+    end
+
+    # Register a Docker::Container handler
+    #
+    # * returning nil is equivalent to returning an empty Hash
+    # * nil values are elided
+    # * String values are kept as raw strings
+    # * Object values are encoded to JSON
+    #
+    # @param proc [Proc] Kontena::Registrator::Docker::Container -> Hash{String => nil, String, JSON>
+    def docker_container(proc)
+      @docker_container = proc
+    end
+  end
+
+  # Normalize policy nodes to etcd nodes
+  #
+  # @param nodes [Hash{String => nil, String, JSON}]
+  # @return nodes [Hash{String => String}]
+  def apply_nodes(nodes)
+    return {} if nodes.nil?
+
+    Hash[nodes.map{|key, value|
+      case value
+      when nil
+        next
+      when String
+        [key, value]
+      when true, false, Integer, Float, Array, Hash
+        [key, value.to_json]
+      else
+        raise TypeError, "Invalid value for etcd #{key}: #{value.inspect}"
+      end
+    }]
+  end
+
+  # Compile a Docker::State into a set of etcd nodes
+  #
+  # @param state [Kontena::Registrator::Docker::State]
+  # @return [Hash<String, String>] nodes for Kontena::Etcd::Writer
+  def apply(state)
+    nodes = {}
+
+    state.containers.each do |container|
+      nodes.merge! apply_nodes @context[:docker_container].call(container)
+    end
+
+    nodes
   end
 end
--- lib/kontena/registrator/service.rb
+++ lib/kontena/registrator/service.rb
@@ -3,65 +3,61 @@ require 'celluloid'
 require 'kontena/logging'
 require 'kontena/etcd'
 
-module Kontena
-  class Registrator
-    # Apply a Policy against Docker::State and update Kontena::Etcd::Writer
-    class Service
-      include Kontena::Logging
-      include Celluloid
+# Apply a Policy against Docker::State and update Kontena::Etcd::Writer
+class Kontena::Registrator::Service
+  include Kontena::Logging
+  include Celluloid
 
-      ETCD_TTL = 30
+  ETCD_TTL = 30
 
-      # @param docker_observable [Kontena::Observable<Kontena::Registrator::Docker::State>]
-      # @param policy [Kontena::Registrator::Policy]
-      # @param start [Boolean] autostart when supervised, set to false for test cases
-      def initialize(docker_observable, policy, start: true)
-        @docker_observable = docker_observable
-        @etcd_writer = Kontena::Etcd::Writer.new(ttl: ETCD_TTL)
-        @policy = policy
+  # @param docker_observable [Kontena::Observable<Kontena::Registrator::Docker::State>]
+  # @param policy [Kontena::Registrator::Policy]
+  # @param start [Boolean] autostart when supervised, set to false for test cases
+  def initialize(docker_observable, policy, start: true)
+    @docker_observable = docker_observable
+    @etcd_writer = Kontena::Etcd::Writer.new(ttl: ETCD_TTL)
+    @policy = policy
 
-        self.start if start
-      end
+    self.start if start
+  end
 
-      # Start update+refresh loop
-      def start
-        refresh_interval = @etcd_writer.ttl / 2
-        logger.info "refreshing etcd every #{refresh_interval}s..."
+  # Start update+refresh loop
+  def start
+    refresh_interval = @etcd_writer.ttl / 2
+    logger.info "refreshing etcd every #{refresh_interval}s..."
 
-        self.async.run
+    self.async.run
 
-        # Run a refresh loop to keep etcd nodes alive
-        every(refresh_interval) do
-          self.refresh
-        end
-      end
-
-      # Apply @policy, and update etcd
-      #
-      # @param docker_state [Docker::State]
-      def update(docker_state)
-        etcd_nodes = @policy.apply(docker_state)
-
-        logger.info "Update with Docker #containers=#{docker_state.containers.size} => etcd #nodes=#{etcd_nodes.size}"
-
-        @etcd_writer.update(etcd_nodes)
-      end
-
-      # Loop on Docker::State and update
-      def run
-        logger.debug "observing #{@docker_observable}"
-
-        @docker_observable.observe do |docker_state|
-          self.update(docker_state)
-        end
-      end
-
-      # Refresh etcd nodes written by update
-      #
-      # Runs concurrently with run -> update
-      def refresh
-        @etcd_writer.refresh
-      end
+    # Run a refresh loop to keep etcd nodes alive
+    every(refresh_interval) do
+      self.refresh
     end
+  end
+
+  # Apply @policy, and update etcd
+  #
+  # @param docker_state [Docker::State]
+  def update(docker_state)
+    etcd_nodes = @policy.apply(docker_state)
+
+    logger.info "Update with Docker #containers=#{docker_state.containers.size} => etcd #nodes=#{etcd_nodes.size}"
+
+    @etcd_writer.update(etcd_nodes)
+  end
+
+  # Loop on Docker::State and update
+  def run
+    logger.debug "observing #{@docker_observable}"
+
+    @docker_observable.observe do |docker_state|
+      self.update(docker_state)
+    end
+  end
+
+  # Refresh etcd nodes written by update
+  #
+  # Runs concurrently with run -> update
+  def refresh
+    @etcd_writer.refresh
   end
 end
